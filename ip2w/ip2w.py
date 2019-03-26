1756fc5a1c63b1cf31090cc53b7a28f7b1a2d27f
ILYA <ilya.aurov@gmail.com>
2019-03-26 08:34:02 +0300

    update> - Добавил таймауты и реконнект для get (сделал отдельную функцию) - Разделил главную функцию на отдельные ф-ии - получение координат по IP и получение погоды по координатам

--- ip2w/ip2w.py
+++ ip2w/ip2w.py
@@ -11,6 +11,8 @@ DEFAULT_CONFIG = {
     'logging_level': logging.INFO,
     'logfile': None,
     'log_format': {'format': '[%(asctime)s] %(levelname).1s %(message)s', 'datefmt': '%Y.%m.%d %H:%M:%S'},
+    'TIMEOUT': 15,
+    'RECONNECT_ATTEMPTS': 5
 }
 cfg_path = '/usr/local/etc/ip2w.conf'
 ipinfo_endpoint = "http://ipinfo.io/{}/geo?token={}"
@@ -26,6 +28,82 @@ response_headers_bad = [
 pattern = re.compile(r'^\/ip2w\/(?P<addr>\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3})')
 
 
+class WeatherAppException(ValueError):
+    pass
+
+
+def get(arguments, reconnect_attempts, timeout):
+    """
+    Обёртка над requests.get с добавлением попыток реконнекта и таймаутом
+    :param arguments:
+    :param int reconnect_attempts:
+    :param int timeout:
+    :return
+    """
+    for _ in range(reconnect_attempts):
+        result = requests.get(arguments, timeout=timeout)
+        if result:
+            return result
+    raise WeatherAppException("Reconnect attempts exceeded")
+
+
+def get_coordinates_by_ip(environ, config):
+    """
+    Функция определяет координаты по  IP адресу
+    :param dict environ:
+    :param dict config:
+    :return tuple (latitude, longitude):
+    """
+    try:
+        ip = pattern.match(environ["REQUEST_URI"])
+        if ip:
+            ip = ip.groupdict().get('addr')
+        else:
+            raise WeatherAppException("Request doesn't have a valid IP address")
+
+        ipinfo_token = config['ipinfo_token']
+        logging.info("Getting info about ip: {}".format(ip))
+        ipaddr_resp = get(ipinfo_endpoint.format(ip, ipinfo_token))
+        logging.debug("Info about IP: {}".format(ipaddr_resp))
+        ipaddr_resp = json.loads(ipaddr_resp.content)
+        lat, lon = ipaddr_resp["loc"].split(',')
+        return lat, lon
+    except Exception as error:
+        logging.error("We have error while trying to get info about IP: {}".format(error))
+        raise WeatherAppException(error)
+
+
+def get_weather_info_by_coordinates(coord, config):
+    """
+    Функция определяет погоду по координатам
+    :param tuple coord:
+    :param dict config:
+    :return dict: Словарь с ключами city, temp, conditions
+    """
+    try:
+        lat, lon = coord
+        logging.info("Getting info about weather.")
+        weather_token = config['weather_token']
+        weather_resp = get(weather_endpoint.format(lat, lon, weather_token))
+        logging.debug("Weather service response: {}".format(weather_resp))
+    except Exception as error:
+        logging.error("We have error while trying to get info about weather: {}".format(error))
+        raise WeatherAppException(error)
+
+    weather = json.loads(weather_resp.content)
+    response_dict = dict()
+
+    try:
+        response_dict["city"] = weather['name']
+        response_dict["temp"] = "{:.2f}".format(weather['main']['temp'] + kelvin)
+        response_dict["conditions"] = weather['weather'][0]['main']
+        return response_dict
+
+    except Exception as error:
+        logging.error("We have error while trying to parse weather response: {}".format(error))
+        raise WeatherAppException(error)
+
+
 def application(environ, start_response):
     """
     Приложение определяющее погоду по IP адресу
@@ -35,58 +113,27 @@ def application(environ, start_response):
     """
     try:
         config = update_config(cfg_path, DEFAULT_CONFIG)
-        ipinfo_token = config['ipinfo_token']
         logging.basicConfig(filename=config['logfile'], level=config['logging_level'],
                             format=config['log_format']['format'],
                             datefmt=config['log_format']['datefmt'])
-        ip = pattern.match(environ["REQUEST_URI"])
-        if ip:
-            ip = ip.groupdict().get('addr')
-        else:
-            raise ValueError("Request doesn't have a valid IP address")
 
-        logging.info("Getting info about ip: {}".format(ip))
-        ipaddr_resp = requests.get(ipinfo_endpoint.format(ip, ipinfo_token))
-        logging.debug("Info about IP: {}".format(ipaddr_resp))
-        ipaddr_resp = json.loads(ipaddr_resp.content)
-        lat, lon = ipaddr_resp["loc"].split(',')
-    except Exception as error:
-        logging.error("We have error while trying to get info about IP: {}".format(error))
-        start_response(status_bad, response_headers_bad)
-        return [None]
-
-    try:
-        logging.info("Getting info about weather.")
-        weather_token = config['weather_token']
-        weather_resp = requests.get(weather_endpoint.format(lat, lon, weather_token))
-        logging.debug("Weather service response: {}".format(weather_resp))
-    except Exception as error:
-        logging.error("We have error while trying to get info about weather: {}".format(error))
-        start_response(status_bad, response_headers_bad)
-        return [None]
-    weather = json.loads(weather_resp.content)
-    response_dict = dict()
-
-    try:
-        response_dict["city"] = weather['name']
-        response_dict["temp"] = "{:.2f}".format(weather['main']['temp'] + kelvin)
-        response_dict["conditions"] = weather['weather'][0]['main']
-        response_body = json.dumps(response_dict)
+        coord = get_coordinates_by_ip(environ, config)
+        weather = get_weather_info_by_coordinates(coord, config)
+        response_body = json.dumps(weather)
         response_body = response_body.encode()
-    except Exception as error:
-        logging.error("We have error while trying to parse weather response: {}".format(error))
+        # So the content-length is the sum of all string's lengths
+        content_length = len(response_body)
+
+        response_headers = [
+            ('Content-Type', 'application/json'),
+            ('Content-Length', str(content_length))
+        ]
+        start_response(status_ok, response_headers)
+        return [response_body]
+
+    except WeatherAppException:
         start_response(status_bad, response_headers_bad)
         return [None]
-
-    # So the content-length is the sum of all string's lengths
-    content_length = len(response_body)
-
-    response_headers = [
-        ('Content-Type', 'application/json'),
-        ('Content-Length', str(content_length))
-    ]
-    start_response(status_ok, response_headers)
-    return [response_body]
 
 
 def update_config(cfg_path, default_config):
